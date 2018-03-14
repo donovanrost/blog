0756be23fc7d76c9782658abfa68e8a6f8b62d3b
Donovan Rost <drost@ucsc.edu>
2018-03-13 19:26:23 -0700

    updates stuff

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@getIndex' );
 
-Route::get('/about', function () {
-    return view('about');
-});
+
+Route::get('/about', 'PagesController@getAbout' );
+
+Route::get('contact', 'PagesController@getContact');
